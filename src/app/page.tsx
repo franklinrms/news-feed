e782f5590cb05d4ecd3577d321ffd764eaa1d342
Franklin Ramos <franklinramos@outlook.com>
2024-01-25 16:29:52 -0300

    Add configuration files and global styles

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen w-full flex-col p-4">
+      <p>Discover Breaking News! 🔥</p>
+      <Button>Click me</Button>
+    </main>
+  )
+}
