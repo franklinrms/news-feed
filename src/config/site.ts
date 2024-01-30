a6a88143d3334cb86ba016ccc1e36d1b95082153
Franklin Ramos <franklinramos@outlook.com>
2024-01-29 21:42:48 -0300

    Add favicon and manifest files

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'NewsFeed — powered by NewsCatcher API',
+  url: '',
+  ogImage: '',
+  description:
+    'A new news consumption experience, providing relevant information in an efficient and personalized way, dynamically adapting to users constantly evolving interests.',
+  links: {
+    twitter: '',
+    github: 'https://github.com/franklinrms/news-feed',
+  },
+}
+
+export type SiteConfig = typeof siteConfig
