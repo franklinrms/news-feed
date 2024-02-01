94814a00a6d15aaa16dfb82f9f2e01b37281399e
Franklin Ramos <franklinramos@outlook.com>
2024-02-01 16:07:47 -0300

    Add mobile navigation component

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -34,4 +34,35 @@ export const Icons = {
       </defs>
     </svg>
   ),
+  menu: (props: IconProps) => (
+    <svg
+      strokeWidth="1.5"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M3 5H11"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M3 12H16"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+      <path
+        d="M3 19H21"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></path>
+    </svg>
+  ),
 }
